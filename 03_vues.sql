2be54898902e18c83439c03749fc96b71a703f9e
Cocovich <coco@MacBook-Coco.local>
2014-11-20 11:27:19 +0100

    Avancement perso mercredi soir

--- 03_vues.sql
+++ 03_vues.sql
@@ -1,12 +1,9 @@
--- Toriko
+-- ---------------------------------------------------------------------------------
+--                                    Toriko
+-- ---------------------------------------------------------------------------------
+
 -- Historique combats entre 2 dates / P-M
--- Statistiques : Classement P-M selon combat, Liste décédés, Autres...
-
--- P-M
--- Historique dernier combat
--- Statistiques : Nombre monstres combattus/gagnés depuis début/année, Espérance de vie, Autres...
-
--- Tomiko
+-- Procédure pour vérifier nom et date ?
 
 DROP VIEW IF EXISTS projet.historique_combats CASCADE;
 
@@ -14,53 +11,55 @@ CREATE VIEW projet.historique_combats AS
 	SELECT c.*, pm.nom AS "nom_pm", a.nom AS "nom_archetype"
 	FROM projet.combats c
 	INNER JOIN projet.power_mangeurs pm ON c.id_pm = pm.id_pm
-	INNER JOIN projet.archetypes a ON c.id_archetype = a.id_archetype;
+	INNER JOIN projet.archetypes a ON c.id_archetype = a.id_archetype
+	ORDER BY c.date_debut ASC;
 
+-- Classement meilleurs P-M
 
 DROP VIEW IF EXISTS projet.classement_pm CASCADE;
 
 CREATE VIEW projet.classement_pm AS
-	SELECT s.id_pm, pm.nom AS "nom", SUM(s.nb_victoires_annee) AS "victoires"
+	SELECT pm.nom AS "nom", SUM(s.nb_victoires_annee) AS "victoires"
 	FROM projet.statistiques s
 	INNER JOIN projet.power_mangeurs pm ON s.id_pm = pm.id_pm
-	GROUP BY s.id_pm, nom
+	GROUP BY nom
 	ORDER BY victoires DESC;
-	
+
+-- Liste P-M décédés
+
 DROP VIEW IF EXISTS projet.liste_decedes CASCADE;
 
 CREATE VIEW projet.liste_decedes AS
-	SELECT pm.id_pm, pm.nom, pm.date_deces
+	SELECT pm.nom, pm.date_deces
 	FROM projet.power_mangeurs pm
 	WHERE pm.date_deces IS NOT NULL
 		AND EXTRACT( YEAR FROM pm.date_deces ) = EXTRACT( YEAR FROM NOW() );
 
--- Power-Mangeurs
+-- Autres...
+
+-- ---------------------------------------------------------------------------------
+--                                Power-Mangeurs
+-- ---------------------------------------------------------------------------------
+
+-- Historique dernier combat
 
 DROP VIEW IF EXISTS projet.historique_dernier_combat CASCADE;
 
 CREATE VIEW projet.historique_dernier_combat AS
 	SELECT c.id_pm, pu.nom AS "nom_pu", u.date_utilisation, c.date_debut, c.date_fin
 	FROM projet.combats c
-	INNER JOIN projet.utilisations u ON c.id_combat = u.id_combat
-	INNER JOIN projet.power_ups pu ON u.id_pu = pu.id_pu
+	LEFT JOIN projet.utilisations u ON c.id_combat = u.id_combat
+	LEFT JOIN projet.power_ups pu ON u.id_pu = pu.id_pu
 	ORDER BY c.date_debut DESC
 	LIMIT 1;
-	
+
+-- Nombre monstres combattus/gagnés depuis début/année
+
 DROP VIEW IF EXISTS projet.statistiques_combats CASCADE;
 
 CREATE VIEW projet.statistiques_combats AS
-	SELECT s.*, pm.nom AS "nom_pm", a.nom AS "nom_archetype"
+	SELECT s.*, a.nom AS "nom_archetype"
 	FROM projet.statistiques s
-	INNER JOIN projet.power_mangeurs pm ON s.id_pm = pm.id_pm -- Est-ce nécessaire ?
 	INNER JOIN projet.archetypes a ON s.id_archetype = a.id_archetype;
 
--- TESTS
-
-SELECT *
-FROM projet.historique_combats
-WHERE "id_pm" = 2
-	AND "date_debut" = '2014-11-17 20:01:35'
-	AND "date_fin" = '2014-11-17 20:06:25';
-	
-SELECT *
-FROM projet.liste_decedes;
+-- Autres...
--- README.md
+++ README.md
@@ -6,31 +6,39 @@ Plan
 
 À compléter...
 
-1 - DSD							| Semaine 1-2	|   |
-	1 - Tables					| Semaine 1		| X | Badot & Dandoy
-	2 - Vues						| Semaine 3		|   | Badot & Dandoy
-	3 - Procédures				| Semaine 3		|   | Badot & Dandoy
-	4 - Triggers					| 				|   | 
-	5 - Données test				| Semaine 2		|   | 
-	
-2 - Application Java				| Semaine 2-5	|   |
-	1 - Tomiko					| 				|   | 
-		1 - Inscription P-M		| 				|   | 
-		2 - Ajout Archétype		| 				|   | 
-		3 - Statistiques			| 				|   | 
-		4 - Historique			| 				|   | 
-		5 - Attribution P-U		| 				|   | 
-	2 - Power-mangeurs 			| 				|   | 
-		1 - Activation			| 				|   | 
-			1 - Débuter combat	| 				|   | 
-			2 - Utiliser P-U		| 				|   | 
-			3 - Conclure combat	| 				|   | 
-		2 - Historique			| 				|   | 
-		3 - Statistiques			| 				|   | 
-	
-3 - Rapport						| Semaine 5		|   |
-	1 - Code SQL					| 				|   |
-	2 - Données test				| 				|   |
-	3 - Code Java				| 				|   |
-	4 - Justif. & explic.		| 				|   |
-	5 - Conclusion				| 				|   |
+Plan	 | Semaine | Fini | Chargé 
+:--------------------------------|:-----:|:-:|:---------------
+1. DSD							| 1-3 	|   |				 
+    1. Tables					| 1		| X | Badot & Dandoy 
+    2. Vues						| 3		|   | Badot & Dandoy 
+    3. Procédures				| 3		|   | Badot & Dandoy 
+    4. Triggers					| 		|   | 				 
+    5. Données test				| 2		|   | 				 
+2. Application Java				| 2-5	|   | 				 
+	1. Tomiko					| 		|   | 
+		1. Inscription P-M		| 		|   | 
+		2. Ajout Archétype		| 		|   | 
+		3. Statistiques			| 		|   | 
+		4. Historique			| 		|   | 
+		5. Attribution P-U		| 		|   | 
+	2. Power-mangeurs 			| 		|   | 
+		1. Activation			| 		|   | 
+			1. Débuter combat	| 		|   | 
+			2. Utiliser P-U		| 		|   | 
+			3. Conclure combat	| 		|   | 
+		2. Historique			| 		|   | 
+		3. Statistiques			| 		|   | 
+3. Rapport						| 5		|   |
+	1. Code SQL					| 		|   |
+	2. Données test				| 		|   |
+	3. Code Java					| 		|   |
+	4. Justif. & explic.			| 		|   |
+	5. Conclusion				| 		|   |
+
+Questions
+---------
+
+~ Nom unique ?
+
+Remarques
+---------
